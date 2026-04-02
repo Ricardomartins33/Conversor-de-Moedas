52e4e36cd8b1072e3da0958b6f76f778a72e918f
Ricardo Martins <ricardospnogueira31@gmail.com>
2026-04-01 21:56:19 -0300

    Add delay before fetching currency data

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@ async function convertValues() {
     try {
         currenceValorConvertido.innerHTML = "Carregando...";
 
-        
+        await new Promise(resolve => setTimeout(resolve, 500));
        const response = await fetch(`https://api.frankfurter.app/latest?from=BRL`);
         const data = await response.json();
 
